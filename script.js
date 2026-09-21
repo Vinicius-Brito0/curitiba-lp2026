9ab2a94cc3652c72dfbd4e4857a15ad65ea42afb
Vinicius-Brito0 <viniciusbrito@div.tec.br>
2026-09-21 17:28:21 -0300

    Personalização de botões

---
+++ script.js
@@ -0,0 +1,21 @@
+/* let nome = "Pedro"
+let idade = 20
+
+
+if (idade >= 18) {
+    console.log(nome + " é maior de idade.")
+}
+*/
+
+let numero1 = 10
+let numero2 = 20
+let operaçao = 1
+
+if(operacao == 1) {
+    console.log("Sua soma deu: ", numero1 + numero2)
+}
+if(operacao == 2) {
+    console.log("Sua subtração deu: ", numero1 - numero2)
+}
+if(operaçao == 3)
+    console.log("Sua divisão deu: ", numero1 / numero2)
